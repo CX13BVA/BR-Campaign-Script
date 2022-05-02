45feb274925a0ddf01b110a16d3da42e0074d466
Jason X <jasonx13nightcore@gmail.com>
2022-05-02 16:04:10 +0200

    Update rus01.lua

--- rus01.lua
+++ rus01.lua
@@ -3,17 +3,19 @@
 --
 -- Unit ID notes
 -- 2,3,4,5 Defensive posts (will be elimentated)
--- 11,12,13,14,15,16,17 Enemy front spawn when above is defeated. >> delay 8000
+-- 11,12,13,14,15,16,17 Enemy front spawn when above is defeated. Delay == 8000 + (Italian)
 -- 7,8,9 RP units (trigger for reinforcement)
 -- 20,21,22,23,24,25,26,27 Player Reinforcements (Defense)
---
+-- 40,41,42,43,(44,45) Italian forces + reinforcements (German)
 --
 function Init()
-    RunScript("DebugView", 1000);
-    RunScript("ForceAdvanceBegin", 1000)
-    RunScript("EBase", 1000);
-    RunScript("EAdvance", 1500);
-    RunScript("DefReinforce", 2000);
+    RunScript("DebugView", 1000); -- Testmode (Cheats)
+    RunScript("ForceAdvanceBegin", 1000) -- landing enemy reinforcements after 38(t) has died.
+    RunScript("EBase", 1000); -- enemy establishing a base at the village (south-west).
+    RunScript("EAdvance", 1500); -- Enemy assault & reinforcements for the river side.
+    RunScript("DefReinforce", 2000); -- Player reinforcements
+    RunScript("StayAlive", 2000); -- Officer (player) needs to survive
+    RunScript("EAdvanceIta", 2000); -- Italian advance
 end;
 function DebugView() -- Testmode (Cheats)
     Password("Panzerklein");
@@ -23,25 +25,33 @@ function DebugView() -- Testmode (Cheats)
 --    God(0,1)
     Suicide();
  end;
-function ForceAdvanceBegin()
+function Winner() -- Player wins
+    Win(0);
+    Suicide()
+end;
+function Lost() -- Player looses
+    Loose()
+    Suicide()
+end;
+function ForceAdvanceBegin() -- landing enemy assault after 38(t) has died.
     if (GetNUnitsInScriptGroup(10) == 0) then
         LandReinforcement(17);
         LandReinforcement(11);
         Suicide();
     end;
 end;
-function EBase()
+function EBase() -- village taken by enemy => landing enemy reinforcements
     if (GetNUnitsInScriptGroup(2) == 0) or
         (GetNUnitsInArea(0, "Village") == 0) then
         RunScript("ReBase", 8000);
         Suicide();
     end;
 end;
-function ReBase()
+function ReBase() -- reinforcing village with enemy troops for base
     LandReinforcement(12);
     Suicide();
 end;
-function EAdvance()
+function EAdvance() -- engaging enemy advance (assault below)
     RunScript("EAAdvance3", 1000);
     RunScript("EAAdvance4", 1000);
     RunScript("EAAdvance5", 1000);
@@ -103,11 +113,56 @@ function DefReinforce() -- Player reinforcements
     RunScript("T26Rein", 1000)
     Suicide();
 end;
-function T26Rein()
+function T26Rein() -- T26 reinforcement from repair factory
     if (GetNScriptUnitsInArea(9, "Car") > 0) then
         ChangePlayer(9, 0);
         LandReinforcement(20);
         LandReinforcement(21);
+        Suicide();
+    end;
+end;
+-- 40,41,42,43,(44,45,46) Italian forces + reinforcements (German)
+function EAdvanceIta() -- Italian forces spawn
+    if (GetNUnitsInScriptGroup(2) == 0) or
+        (GetNUnitsInArea(0, "Village") == 0) and
+        (GetNUnitsInScriptGroup(3) == 0) or
+        (GetNUnitsInArea(0, "CentralBridge") == 0) and
+        (GetNUnitsInScriptGroup(4) == 0) or
+        (GetNUnitsInArea(0, "RailFactory") == 0) and
+        (GetNUnitsInScriptGroup(5) == 0) or
+        (GetNUnitsInArea(0, "FactoryCom") == 0) then
+        RunScript("ItalianAdvance", 10000);
+        Suicide();
+    end;
+end;
+function ItalianAdvance() -- Italian forces secure their base
+    RunScript("ItalianBase", 1000);
+    LandReinforcement(40);
+    Suicide();
+end;
+function ItalianBase() -- Italian base being reinforced, RG-41,44
+        if (GetNUnitsInArea(0, "ItaVillage") == 0) then
+        LandReinforcement(41);
+        LandReinforcement(44);
+        RunScript("ItalianBaseSec", 4000);
+        RunScript("PrepItalianAssault", 10000);
+        Suicide();
+    end;
+end;
+function ItalianBaseSec() -- RG-46
+    LandReinforcement(46)
+    Suicide();
+end;
+function PrepItalianAssault() -- RG-42
+    LandReinforcement(42)
+    RunScript("ItaWave1", 1000);
+    Suicide();
+end;
+function ItaWave1() -- RG-42,43
+    if (GetNUnitsInScriptGroup(42) == 0) then
+        RunScript("ItaWave2", 1000);
+        LandReinforcement(42);
+        LandReinforcement(43);
         Suicide();
     end;
 end;
